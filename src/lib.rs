e51887807912e46fcd4655d644fca5661e3853ed
Murtaza Akbari <murtazasakbari@gmail.com>
2024-02-17 16:36:09 +0100

    Fetch op

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,26 @@ const NUM_KEYS: usize = 16;
 
 const START_RAM_ADDR: u16 = 0x200;
 
+const FONTSET_SIZE: usize = 80;
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
 pub struct Emulator {
     program_counter: u16,
     ram: [u8; RAM_SIZE],
@@ -23,6 +43,17 @@ pub struct Emulator {
 
 impl Emulator {
     pub fn new() -> Self {
+        let mut emulator = Self::default_emulator_state();
+        emulator.initialize_ram();
+        emulator
+    }
+
+    pub fn reset(&mut self) {
+        *self = Self::default_emulator_state();
+        self.initialize_ram();
+    }
+
+    fn default_emulator_state() -> Self {
         Self {
             program_counter: START_RAM_ADDR,
             ram: [0; RAM_SIZE],
@@ -35,5 +66,63 @@ impl Emulator {
             delay_timer: 0,
             sound_timer: 0,
         }
+    }
+
+    fn initialize_ram(&mut self) {
+        self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+    }
+
+    fn push(&mut self, val: u16) {
+        self.stack[self.stack_pointer as usize] = val;
+        self.stack_pointer += 1;
+    }
+
+    fn pop(&mut self) -> u16 {
+        self.stack_pointer -= 1;
+        self.stack[self.stack_pointer as usize]
+    }
+
+    fn fetch(&mut self) -> u16 {
+        let higher_byte = self.ram[self.program_counter as usize] as u16;
+        let lower_byte = self.ram[(self.program_counter + 1) as usize] as u16;
+        let op = (higher_byte << 8) | lower_byte;
+        self.program_counter += 2;
+        op
+    }
+
+    pub fn tick_timers(&mut self) {
+        if self.delay_timer > 0 {
+            self.delay_timer -= 1;
+        }
+        if self.sound_timer > 0 {
+            if self.sound_timer == 1 {}
+            self.sound_timer -= 1;
+        }
+    }
+
+    pub fn tick(&mut self) {
+        let op = self.fetch();
+        //self.execute(op);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Emulator;
+
+    #[test]
+    fn stack_ops() {
+        let mut emulator = Emulator::new();
+        Emulator::push(&mut emulator, 1);
+        Emulator::push(&mut emulator, 2);
+        Emulator::pop(&mut emulator);
+
+        assert_eq!(emulator.stack[0..3], [1u16, 2, 0]);
+    }
+
+    #[test]
+    fn test_fetch_op() {
+        let mut emulator = Emulator::new();
+        assert_eq!(emulator.fetch(), 0);
     }
 }
